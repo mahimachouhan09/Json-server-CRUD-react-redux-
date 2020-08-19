4d45cbbb5179af4918d60993b70ae17bbf219d28
mahimachouhan <mchouhan@bestpeers.com>
2020-08-19 09:47:23 +0530

    json server initial commit

---
+++ src/components/updateUserForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class updateUserForm extends Component {
+  render() {
+    return (
+      <div>
+        update entries
+      </div>
+    )
+  }
+}
+
+export default updateUserForm
